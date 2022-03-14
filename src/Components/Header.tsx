cdf40c2fa0eefbf51da3b9e005afd2103eaa4c25
Vadim3618 <vdmdzr@gmail.com>
2022-03-14 15:00:53 +0300

    13 lesson

---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <header className={'header'}>
+      <img src="https://stonerussia.ru/images/upload/202101041536080.png"/>
+    </header>
+
+  );
+};
+
---
+++ src/Components/Profile.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className={'content'}>
+      <div>
+        <img src="https://i.ucrazy.ru/files/i/2007.12.3/1196691095_getphoto168.jpg" alt=""/>
+      </div>
+      <div>
+        ava+d
+      </div>
+      <div>
+        My posts
+        <div>
+          New post
+        </div>
+        <div>
+          <div>post1</div>
+          <div>post2</div>
+        </div>
+      </div>
+    </div>
+
+  );
+};
+
+export default Profile;
